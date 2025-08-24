67f7ea6e66342465dad49e41fb8d2aed92992d9d
Nicholas Bollweg <nick.bollweg@gmail.com>
2025-08-24 08:50:07 -0500

    fix version comparison

--- recipe/check-go-version.sh
+++ recipe/check-go-version.sh
@@ -4,7 +4,7 @@ set -eux
 echo "ensuring go version in go.mod:#/go is ${CBC_GO_VERSION}"
 GO_MOD_GO_VERSION=$(grep -iE "^go .*$" go.mod)
 
-if [[ "${GO_MOD_GO_VERSION}" == "go ${GO_MOD_GO_VERSION}" ]] ;
+if [[ "${GO_MOD_GO_VERSION}" == "go ${CBC_GO_VERSION}" ]] ;
 then
   echo "OK go version in go.toml and conda_build_config.yaml agree: ${GO_MOD_GO_VERSION}"
 else
